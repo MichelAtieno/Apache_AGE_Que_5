36d6a97fa4672263b5f3bcee9b238e1c88ff016d
Michel Atieno <michelatieno23@gmail.com>
2023-08-17 12:29:59 +0300

    Added login functionality, however messages not showing yet

--- apache_age_app/urls.py
+++ apache_age_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, login_user
 
 app_name="apache_age_app"
 
 urlpatterns = [
     path('', home, name="home"),
+    path('login_user',login_user, name="login"),
 ]
--- apache_age_app/views.py
+++ apache_age_app/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('home')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, ("There was an error logging in. Try again."))
+            return redirect('login')
+    else:
+        context = {
+
+        }
+    
+        return render(request, 'registration/login.html', context)
