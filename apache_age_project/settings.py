d8ef47a09c191d9ba1fd7ee5ec1643f29f3dc479
Michel Atieno <michelatieno23@gmail.com>
2023-08-24 17:26:22 +0300

    Removed unnecessary text

--- apache_age_project/settings.py
+++ apache_age_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-9d_380bwbb)-s2&bd6#-xv+3k34jyvs+9l@==6oi71lxdak7et'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -32,10 +32,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-# DEBUG = True
-
-# ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -99,7 +95,6 @@ database_url = os.environ.get("DATABASE_URL")
 
 DATABASES["default"] = dj_database_url.parse(database_url)
 
-# postgres://bitnine_que_5_db_user:OCw71WNF7PfnHKLKFeEoWJmrwvhlOuhf@dpg-cjjlus0cfp5c73b6stpg-a.oregon-postgres.render.com/bitnine_que_5_db
 
 
 # Password validation
